973cb06ac6e8bd2894f6989d4f8b951ab0eb5a0a
Talibé-Barry <50770479+Tony-Belita@users.noreply.github.com>
2025-09-26 13:12:44 +0200

    exemple du fichier de données des électeurs à adapter au contexte

---
+++ database_example.js
@@ -0,0 +1,36 @@
+const electoralData = {
+  "BV-1": [
+    {
+      "numero": 1,
+      "nom": "HENRY",
+      "prenom": "Christophe",
+      "date_naissance": "07/12/1995",
+      "sexe": "Masculin",
+      "profession": "Peintre"
+    },
+    {
+      "numero": 2,
+      "nom": "FERDINAND",
+      "prenom": "jean",
+      "date_naissance": "15/06/1992",
+      "sexe": "Masculin",
+      "profession": "Ingénieur informaticien"
+    },
+    {
+      "numero": 3,
+      "nom": "LEMERCIER",
+      "prenom": "Alex",
+      "date_naissance": "10/06/2003",
+      "sexe": "Masculin",
+      "profession": "Jardinier"
+    }
+    ],
+  "BV-2": [
+    { // jeu de données pour le bureau 2 
+      }
+    ],
+   "BV-3": [
+    { // jeu de données pour le bureau 3
+      }
+    ]
+}
